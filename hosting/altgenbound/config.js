0c5169bf880f44cfb33a1ce03c1c81981329c5da
A real Saber <ultrasaber@users.noreply.github.com>
2019-09-21 16:23:44 -0500

    deploy altgenbound again

--- hosting/altgenbound/config.js
+++ hosting/altgenbound/config.js
@@ -5,5 +5,19 @@ var SburbConfig = {
   "chooserFill2": "#7289DA",
   "chooserFill3": "#23272A",
   "chooserCursor": "#ffffff",
-  "chooserCursorSelected": "#cccccc"
+  "chooserCursorSelected": "#cccccc",
+
+  "prefixColours": {
+    "dickle":"#3478ED",
+    "node":"#FF00FF",
+    "guy":"#1C1CED",
+    "cheeto":"#208E99",
+    "swifter":"#FF2364",
+    "holly":"#E3ABEA",
+    "liz":"#e83bff",
+    "vice":"#cc0a0a",
+    "vrachne":"#1025C3",
+    "bowman":"#720720",
+    "makin":"#AA72C0"
+  }
 }
