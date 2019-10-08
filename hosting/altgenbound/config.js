54116869cad6017f541521c7bcf2e07f8384de96
A real Saber <ultrasaber@users.noreply.github.com>
2019-10-08 12:50:48 -0500

    deploy

--- hosting/altgenbound/config.js
+++ hosting/altgenbound/config.js
@@ -19,5 +19,48 @@ var SburbConfig = {
     "vrachne":"#1025C3",
     "bowman":"#720720",
     "makin":"#AA72C0"
+  },
+
+  "characterAnimation": {
+    "idleFront": {
+      "startPos": 0,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "idleRight": {
+      "startPos": 1,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "idleBack": {
+      "startPos": 2,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "idleLeft": {
+      "startPos": 3,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "walkFront": {
+      "startPos": 8,
+      "length": 4,
+      "frameInterval": 4
+    },
+    "walkRight": {
+      "startPos": 12,
+      "length": 4,
+      "frameInterval": 4
+    },
+    "walkBack": {
+      "startPos": 16,
+      "length": 4,
+      "frameInterval": 4
+    },
+    "walkLeft": {
+      "startPos": 20,
+      "length": 4,
+      "frameInterval": 4
+    }
   }
 }
